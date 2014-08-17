5779f44a943d34f86f88162ce9ca0499a0e50fac
aniketharshe20 <aniketharshe20@gmail.com>
2014-08-17 14:44:11 +0530

    new changes

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -46,4 +46,29 @@ class Profile extends CI_Controller {
         $data['view'] = "edit";
         $this->load->view('content', $data);
     }
+
+    public function changepassword()
+    {
+
+        $post = $this->input->post();
+        if ($post) {
+            $this->form_validation->set_rules('old_password', 'Old password', 'trim|required');
+            $this->form_validation->set_rules('new_password', 'New password', 'trim|required');
+            $this->form_validation->set_rules('confirm_password', 'Confirm password', 'trim|required|matches[new_password]');
+
+            if ($this->form_validation->run()) {
+                $data = array('Password' => md5($post['new_password']) );
+                $ret = $this->common_model->updateData(TBLUSER, $data, array('id' => $this->front_session['id']) );
+                if ($ret > 0) {
+                    $data['flash_msg'] = 'Password changed successfully.';
+                }else{
+                   $data['flash_msg'] = 'An error occurred while processing.';
+                }
+
+            }
+        }
+
+        $data['view'] = "change_password";
+        $this->load->view('content', $data);
+    }
 }
